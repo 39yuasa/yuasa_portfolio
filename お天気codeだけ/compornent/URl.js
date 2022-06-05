2c50d0fee01860a58049fc77702939c7a9664296
39yuasa <85480451+39yuasa@users.noreply.github.com>
2022-06-06 02:58:35 +0900

    Delete お天気codeだけ directory

--- お天気codeだけ/compornent/URl.js
+++
@@ -1,5 +0,0 @@
-const weatherURL = (domain) => (parameter) => domain + parameter;
-const DomainURL = weatherURL(
-  "https://www.jma.go.jp/bosai/forecast/data/forecast/"
-);
-export default DomainURL;
